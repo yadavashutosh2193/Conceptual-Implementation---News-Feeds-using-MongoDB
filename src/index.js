c8325244c86824eb6907ce0203a5106f8927e102
Win-10 <yadavashutosh2193@gmail.com>
2020-12-19 10:11:07 +0530

    done bibhuti code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const { data } = require('./data');
+const express = require('express')
 const app = express()
 const port = 8080
+const { newsArticleModel } = require('./connector');
 
 const onePageArticleCount = 10
 
-const {newsArticleModel} = require('./connector');
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// const getval = (value, defaultvalue)=>
-//      (value === null || value === undefined || isNaN(Number(value))) ? defaultvalue : Number(value);
 
-app.get("/newFeeds", async (req, res)=>{
-  //   const offset = getval(req.query.offset, 0);
-  //   const limit = getval(req.query.limit, onePageArticleCount);
-  //   const datafromServer = await newsArticleModel.find().skip(offset).limit(limit);
-  // res.status(200).send(datafromServer);
-  res.send(await newsArticleModel.find().skip(Number(req.query.offset) || 0).limit(Number(req.query.limit) || 10))
-})
+//const isNullOrUndefinedOrNan = (val)=> val !==null || val !==undefined || isNan(val);
+app.get('/newFeeds', async (req, res) => {
+    // const offset = isNullOrUndefinedOrNan(req.query.offset) ? 0 : Number(req.query.offset);
+    // const limit = isNullOrUndefinedOrNan(req.query.limit) ? 10 : Number(req.query.limit);
+    res.send(await newsArticleModel.find().skip(Number(req.query.offset) || 0).limit(Number(req.query.limit) || 10));
+
+});
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 

9e6e56ed4f8471cac4f5e8f7a7aeac131dabe977
Win-10 <yadavashutosh2193@gmail.com>
2020-12-19 09:46:15 +0530

    done now

--- src/index.js
+++ src/index.js
@@ -9,14 +9,15 @@ const {newsArticleModel} = require('./connector');
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const getval = (value, defaultvalue)=>
-     (value === null || value === undefined || isNaN(Number(value))) ? defaultvalue : Number(value);
+// const getval = (value, defaultvalue)=>
+//      (value === null || value === undefined || isNaN(Number(value))) ? defaultvalue : Number(value);
 
 app.get("/newFeeds", async (req, res)=>{
-    const offset = getval(req.query.offset, 0);
-    const limit = getval(req.query.limit, onePageArticleCount);
-    const datafromServer = await newsArticleModel.find().skip(offset).limit(limit);
-  res.status(200).send(datafromServer);
+  //   const offset = getval(req.query.offset, 0);
+  //   const limit = getval(req.query.limit, onePageArticleCount);
+  //   const datafromServer = await newsArticleModel.find().skip(offset).limit(limit);
+  // res.status(200).send(datafromServer);
+  res.send(await newsArticleModel.find().skip(Number(req.query.offset) || 0).limit(Number(req.query.limit) || 10))
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
